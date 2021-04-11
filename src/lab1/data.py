3146cdcccc21c0d69a4cc8c1f2aab283e86d6e05
katiadias <81766182+katiadias@users.noreply.github.com>
2021-04-11 23:58:01 +0100

    Update data.py

---
+++ src/lab1/data.py
@@ -0,0 +1,20 @@
+class Data:
+    def __init__(self,ano,mes,dia):
+        self.ano = ano
+        self.mes = mes
+        self.dia = dia
+        
+        
+    def  obter_data(self):
+        data= (self.dia,self.mes,self.ano)
+        return data
+    
+        def obter_ano (self):
+            return self.ano
+    
+        def obter_mes (self):
+            return self.mes
+    
+        def obter_dia (self):
+            return self.dia
+    
